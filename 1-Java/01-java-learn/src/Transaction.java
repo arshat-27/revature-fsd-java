6560bcf4a859532c1c28180ba1b1357d89a96b60
Arshatullah Mohammed <arshuprivate@gmail.com>
2021-09-02 10:31:56 +0530

    Push Successfull

--- 1-Java/01-java-learn/src/Transaction.java
+++ 1-Java/01-java-learn/src/Transaction.java
@@ -1,21 +1,28 @@
-
 public class Transaction {
 	
 	private char type;
 	private double amount;
-	
-	public Transaction(char type,double amount) {
+	//constructor
+	public Transaction(char type, double amount) {
 		this.type = type;
 		this.amount = amount;
 	}
+	//instance 
 	public void displayTransaction(double balance) {
-		System.out.printf("%4c %12.2f %8.2f\n" ,type,amount,balance);
-
+		System.out.printf("   %c %12.2f %8.2f\n", type, amount, balance);
 	}
-	
 	public static void main(String args[]) {
-		Transaction transaction= new Transaction('W',5000);
-		transaction.displayTransaction(2000);
+		Transaction transaction1 = new Transaction('W', 2000);
+		Transaction transaction2 = new Transaction('W', 20000);
+		Transaction transaction3 = new Transaction('W', 500);
+		Transaction transaction4 = new Transaction('W', 15500);
+		Transaction transaction5 = new Transaction('W', 10000);
+		
+		Transaction[] transactionArray= {transaction1,transaction2, transaction3, transaction4, transaction5 };
+		
+	       for(Transaction transaction : transactionArray) {
+	    	   transaction.displayTransaction(5000);
+	       }
 	}
 
 }
